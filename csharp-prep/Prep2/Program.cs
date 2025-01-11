c5650a65dc8db81894b933876bf2211130cfef3a
Coolbou0427 <58377572+Coolbou0427@users.noreply.github.com>
2025-01-11 14:47:55 -0800

    W01 Prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage: ");
+        Double grade = Double.Parse(Console.ReadLine());
+        String letter;
+        if (grade >= 90)
+        {
+            letter = ("A");
+        }
+        else if (grade >= 80)
+        {
+            letter = ("B");
+        }
+        else if (grade >= 70)
+        {
+            letter = ("C");
+        }
+        else if (grade >= 60)
+        {
+            letter = ("D");
+        }
+        else if (grade < 60)
+        {
+            letter = ("F");
+        }
+        else
+        {
+            letter = (null);
+        }
+        Console.Write($"You recieved a {letter}. ");
+        if (grade >= 70)
+        {
+            Console.Write("You passed! Congratulations!");
+        }
+        else
+        {
+            Console.Write("You did not pass, but don't worry. You got it next time!");
+        }
     }
 }
