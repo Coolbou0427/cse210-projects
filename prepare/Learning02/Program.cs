b8d0e7876b4cd5735e1512ab3556f63d4a579596
Coolbou0427 <58377572+Coolbou0427@users.noreply.github.com>
2025-01-22 17:34:04 -0800

    Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job Job1 = new Job();
+        Job1._company = "Blizzard";
+        Job1._startYear = 2005;
+        Job1._endYear = 2015;
+        Job1._jobTitle = "Software Engineer";
+        
+        Job Job2 = new Job();
+        Job2._company = "Take Two Interactive";
+        Job2._startYear = 2015;
+        Job2._endYear = 2020;
+        Job2._jobTitle = "Lead Project Debugger";
+
+        Job1.Display();
+        Job2.Display();
+    
+        Resume myResume = new Resume();
+        myResume._name = "Jaxon Terrill";
+
+        myResume._jobs.Add(Job1);
+        myResume._jobs.Add(Job2);
+
+        myResume.Display();
     }
 }
