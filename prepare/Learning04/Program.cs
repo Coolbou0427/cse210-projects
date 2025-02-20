1145fb73d2670ee324a19ed401328785ac32d305
Coolbou0427 <58377572+Coolbou0427@users.noreply.github.com>
2025-02-20 15:09:55 -0800

    Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Jaxon Terrill", "Division");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Jaxon Terrill", "Pythagorean Theorem", "8.1", "10-20");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Jaxon Terrill", "Personal Narrative", "My life story");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
