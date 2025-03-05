7e596caa5014c7df7720d49044d4e1cbc6ee96bb
Coolbou0427 <58377572+Coolbou0427@users.noreply.github.com>
2025-03-05 13:22:56 -0800

    Polymorphism

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,89 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public abstract class Shape
 {
-    static void Main(string[] args)
+    private string _color;
+
+    public Shape(string color)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
+
+public class Square : Shape
+{
+    private double _side;
+
+    public Square(string color, double side) : base(color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
+
+public class Rectangle : Shape
+{
+    private double _length;
+    private double _width;
+
+    public Rectangle(string color, double length, double width) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 3));
+        shapes.Add(new Rectangle("Blue", 4, 5));
+        shapes.Add(new Circle("Green", 6));
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+        }
     }
 }
